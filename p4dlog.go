093a428ade68fba3cc8e896fc2a54d4b36223f49
Robert Cowham <rcowham@perforce.com>
2020-01-08 09:53:59 +0000

    Previous fix not quite correct! Also make tests pass properly

--- p4dlog.go
+++ p4dlog.go
@@ -534,9 +534,6 @@ func (fp *P4dFileParser) processTrackRecords(cmd *Command, lines [][]byte) {
 		if !bytes.Equal(trackStart, line[:len(trackStart)]) {
 			continue
 		}
-		if len(tableName) == 0 { // One of the special tables - discard track records
-			continue
-		}
 		m := reTrackUsage.FindSubmatch(line)
 		if len(m) > 0 {
 			cmd.setUsage(m[1], m[2], m[3], m[4], m[5], m[6], m[7], m[8])
@@ -550,6 +547,10 @@ func (fp *P4dFileParser) processTrackRecords(cmd *Command, lines [][]byte) {
 		m = reTrackRPC.FindSubmatch(line)
 		if len(m) > 0 {
 			cmd.setRPC(m[1], m[2], m[3], m[4], m[5], m[6], nil, nil)
+			continue
+		}
+		// One of the special tables - discard track records
+		if len(tableName) == 0 {
 			continue
 		}
 		m = reTrackPages.FindSubmatch(line)
