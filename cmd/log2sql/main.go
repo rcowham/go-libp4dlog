08339604b39b637bc5f25f7686e5f03626bab744
Robert Cowham <rcowham@perforce.com>
2020-02-08 15:11:31 +0000

    Add ability to read gzipped files

--- cmd/log2sql/main.go
+++ cmd/log2sql/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"compress/gzip"
 	"context"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -167,6 +169,33 @@ func byteCountDecimal(b int64) string {
 	return fmt.Sprintf("%.1f %cB", float64(b)/float64(div), "kMGTPE"[exp])
 }
 
+func scannerFromFile(reader io.Reader) (*bufio.Scanner, error) {
+	var scanner *bufio.Scanner
+	//create a bufio.Reader so we can 'peek' at the first few bytes
+	bReader := bufio.NewReader(reader)
+
+	testBytes, err := bReader.Peek(64) //read a few bytes without consuming
+	if err != nil {
+		return nil, err
+	}
+	//Detect if the content is gzipped
+	contentType := http.DetectContentType(testBytes)
+
+	//If we detect gzip, then make a gzip reader, then wrap it in a scanner
+	if strings.Contains(contentType, "x-gzip") {
+		gzipReader, err := gzip.NewReader(bReader)
+		if err != nil {
+			return nil, err
+		}
+		scanner = bufio.NewScanner(gzipReader)
+	} else {
+		//Not gzipped, just make a scanner based on the reader
+		scanner = bufio.NewScanner(bReader)
+	}
+
+	return scanner, nil
+}
+
 // Parse single log file - output is sent via logparser channel
 func parseLog(logger *logrus.Logger, logfile string, inchan chan []byte) {
 	var file *os.File
@@ -193,7 +222,8 @@ func parseLog(logger *logrus.Logger, logfile string, inchan chan []byte) {
 	inbuf := make([]byte, maxCapacity)
 	reader := bufio.NewReaderSize(file, maxCapacity)
 	preader := progress.NewReader(reader)
-	scanner := bufio.NewScanner(preader)
+	scanner, err := scannerFromFile(reader)
+	// scanner := bufio.NewScanner(preader)
 	scanner.Buffer(inbuf, maxCapacity)
 
 	// Start a goroutine printing progress
@@ -230,7 +260,8 @@ func getDBName(name string, logfiles []string) string {
 		if len(logfiles) == 0 {
 			name = "logs"
 		} else {
-			name = strings.TrimSuffix(logfiles[0], ".log")
+			name = strings.TrimSuffix(logfiles[0], ".gz")
+			name = strings.TrimSuffix(name, ".log")
 		}
 	}
 	if !strings.HasSuffix(name, ".db") {
